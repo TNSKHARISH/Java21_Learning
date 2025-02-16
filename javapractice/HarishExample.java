3de5498d38f1550b3f467103420a5445d7f1d6fb
Naga Siva Karthikey Harish Tiwari <natiwar@L-HYDQAE7MD6M-M.local>
2025-02-16 08:23:03 +0530

    Var Keyword pracrice

--- javapractice/HarishExample.java
+++ javapractice/HarishExample.java
@@ -2,6 +2,7 @@ package javapractice;
 
 public class HarishExample {
     public static void main(String[] args){
+        // Printing First line of code
         System.out.println("First class and first print");
     }
 }
